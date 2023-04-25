70105dda4c322ee6202287be070af48b211c97cc
marwafd <fadil.marwa17@gmail.com>
2023-04-25 20:52:03 +0200

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,12 +26,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-			{
 				printf("%d, ", j);
-			} else if (j == 98)
-			{
-					printf("%d\n", j);
-			}
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
 }
